52d154dae620ea94839a87f3ff0854cb07986db8
Y-H39 <linemo159@gmail.com>
2023-10-16 23:04:21 +0900

    test: コントローラー各種動作確認

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
-end
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to :posts
+  end
+ end
+
+private
+  def post_params
+    params.require(:post).permit(:post_name, :url, :comment)
+  end
